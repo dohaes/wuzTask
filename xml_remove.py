ea2c0891f92c3bbc736cd45e9ec31e5d85c37209
dohaes <Doha1100367@feng.bu.edu.eg>
2017-04-09 01:16:20 +0200

    python script to remove xml ,html tags

---
+++ xml_remove.py
@@ -0,0 +1,18 @@
+from pandas import *
+from bs4 import BeautifulSoup
+
+desc = []
+job_requirement = []
+
+data_frame = pandas.read_csv("akhermara2.csv",encoding="ISO-8859-1")
+
+for index, row in data_frame.iterrows():
+      soup = BeautifulSoup(row['description'],"lxml")
+      soup2 = BeautifulSoup(row['job_requirements'], "lxml")
+      desc.append(soup.get_text())
+      job_requirement.append(soup2.get_text())
+      print (index ,soup.get_text())
+
+data_frame['description']=desc
+data_frame['job_requirements']=job_requirement
+data_frame.to_csv("Pythonoututf8.csv",encoding="UTF-8", index=False )
